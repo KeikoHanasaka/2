f45bfddc0d09b8b6f93898a59dcffafe4e482144
KeikoHanasaka <cqzzvip@outlook.com>
2025-07-10 19:08:22 +0800

    Update 链接.js

--- 链接.js
+++ 链接.js
@@ -1,7 +1,5 @@
 https://sc.smartconcept.ae/wp-content/plugins/function.php
 https://avaliacao.oficinadasaliancas.com.br/wp_api.php
 https://mail.chrono-auto-ecole.com/galery.php
-http://kvinta.tv/wp-content/themes/pridmag/wp_api.php
-https://mlknscbse.org/wp_api.php
 http://yes-spb.ru/wp-admin/wp_api.php
 https://thesidewaystheory.com.au/wp-admin/wp_api.php
