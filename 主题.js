d22066481d96441568747338acd9951b98537f1d
KeikoHanasaka <cqzzvip@outlook.com>
2025-07-14 17:40:58 +0800

    Update 主题.js

--- 主题.js
+++ 主题.js
@@ -1,3 +1,3 @@
-Apple Storeでのご注文ありがとうございます (注文番号：[%UpLetter(8,8)]JP)
-Appleサービスご利用料金のお支払い確認[%UpLetter(8,8)]JP
-定期購読の更新予定のお知らせ[%UpLetter(8,8)]JP
+Apple Storeでのご注文ありがとうございます (注文番号：[%UpLetter(8)]JP)
+Appleサービスご利用料金のお支払い確認[%UpLetter(8)]JP
+定期購読の更新予定のお知らせ[%UpLetter(8)]JP
